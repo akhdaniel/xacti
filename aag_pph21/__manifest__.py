1467db2ece32bab156208fd7da887513de6a75cf
evansadji <evansadji@gmail.com>
2020-09-08 15:19:24 +0700

    ubah sekuriti

--- aag_pph21/__manifest__.py
+++ aag_pph21/__manifest__.py
@@ -24,8 +24,7 @@
 
     # always loaded
     'data': [
-        # 'security/ir.model.access.csv',
-
+        'security/ir.model.access.csv',
         'views/structure_xacti.xml',
         'views/views.xml',
         'views/templates.xml',
