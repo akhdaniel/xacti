6dd47eaa0ef981a36422b9244e156882d18522c6
akhmad daniel sembiring <akhmad.daniel@gmail.com>
2020-09-15 16:04:38 +0700

    Update test.py

--- test_mssql/test.py
+++ test_mssql/test.py
@@ -6,8 +6,15 @@ password="Postgresql"
 port="1433"
 database="HRD"
 
+#---- connection and create cursor
 cs='DRIVER={ODBC Driver 17 for SQL Server};PORT='+port+';SERVER='+ip_address+';DATABASE='+database+';UID='+user+';PWD='+password
 cnn = pypyodbc.connect(cs, unicode_results=True)
+cur = cnn.cursor()
 
-print(cnn)
+#---- query database
+sql = "select * from ANVIZ_ABS_FOR_ODOO"
+cur.execute(sql)
+records = cur.fetchall()
 
+#---- process results
+print(records)
