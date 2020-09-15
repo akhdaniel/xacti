8ce2315a091bca88ace5ab05248be5f7380a66cc
akhmad daniel sembiring <akhmad.daniel@gmail.com>
2020-09-15 15:42:02 +0700

    Update test.py

--- test_mssql/test.py
+++ test_mssql/test.py
@@ -1,4 +1,5 @@
 import pypyodbc
+
 ip_address="172.17.145.17"
 user="user_postgresql"
 password="Postgresql"
@@ -8,5 +9,5 @@ database="HRD"
 cs='DRIVER={ODBC Driver 17 for SQL Server};PORT='+port+';SERVER='+ip_address+';DATABASE='+database+';UID='+user+';PWD='+password
 cnn = pypyodbc.connect(cs, unicode_results=True)
 
+print(cnn)
 
-print(cur)
