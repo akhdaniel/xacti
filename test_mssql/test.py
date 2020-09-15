e7dbc237d1f792c6b5460f1c8ccaef5805012165
akhmad daniel sembiring <akhmad.daniel@gmail.com>
2020-09-15 16:37:18 +0700

    Update test.py

--- test_mssql/test.py
+++ test_mssql/test.py
@@ -12,32 +12,9 @@ cnn = pypyodbc.connect(cs, unicode_results=True)
 cur = cnn.cursor()
 
 #---- query database
-sql = "select IDNO,CHECKTIME_IN,CHECKTIME_OUT from ANVIZ_ABS_FOR_ODOO"
+sql = "select IDNO,CHECKTIME_IN,CHECKTIME_OUT from ANVIZ_ABS_FOR_ODOO where CHECKTIME_IN IS NOT NULL"
 cur.execute(sql)
 records = cur.fetchall()
-
-"""
-[id]
-      ,[LOC_IN]
-      ,[LOC_OUT]
-      ,[IDNO]
-      ,[NAME]
-      ,[CHECKTIME_IN]
-      ,[CHECKTIME_OUT]
-      ,[IN]
-      ,[OUT]
-      ,[RMK]
-      ,[FINAL_RMK]
-      ,[DESC]
-      ,[SDESC]
-      ,[SHIFT]
-      ,[DT_TIME]
-      ,[TIMEINWCA]
-      ,[TIMEOUTWCA]
-      ,[GRPDSC]
-"""
-
-
 
 #---- process results
 sql = """insert into hr_attendance (employee_id, check_in, check_out) VALUES """
