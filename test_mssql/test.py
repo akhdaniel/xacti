99771712853b81fabd3a31ee53fd3c1b29758f5c
akhmad daniel sembiring <akhmad.daniel@gmail.com>
2020-09-15 16:22:14 +0700

    Update test.py

--- test_mssql/test.py
+++ test_mssql/test.py
@@ -12,9 +12,40 @@ cnn = pypyodbc.connect(cs, unicode_results=True)
 cur = cnn.cursor()
 
 #---- query database
-sql = "select * from ANVIZ_ABS_FOR_ODOO"
+sql = "select IDNO,CHECKTIME_IN,CHECKTIME_OUT from ANVIZ_ABS_FOR_ODOO"
 cur.execute(sql)
 records = cur.fetchall()
 
+"""
+[id]
+      ,[LOC_IN]
+      ,[LOC_OUT]
+      ,[IDNO]
+      ,[NAME]
+      ,[CHECKTIME_IN]
+      ,[CHECKTIME_OUT]
+      ,[IN]
+      ,[OUT]
+      ,[RMK]
+      ,[FINAL_RMK]
+      ,[DESC]
+      ,[SDESC]
+      ,[SHIFT]
+      ,[DT_TIME]
+      ,[TIMEINWCA]
+      ,[TIMEOUTWCA]
+      ,[GRPDSC]
+"""
+
 #---- process results
-print(records)
+sql = """insert into hr_attendance (employee_id, check_in, check_out) VALUES """
+values= []
+for rec in records:
+    IDNO = rec[0]
+    CHECKTIME_IN=rec[1]
+    CHECKTIME_OUT=rec[2]
+    values.append( (IDNO, CHECKTIME_IN, CHECKTIME_OUT) )
+
+sql = sql + ",".join(values)
+
+print(sql)
