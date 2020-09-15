bac4720c3da52167c413f52ca0c89759015223e8
akhmad daniel sembiring <akhmad.daniel@gmail.com>
2020-09-15 15:18:17 +0700

    Create test.py

---
+++ test_mssql/test.py
@@ -0,0 +1,12 @@
+import pypyodbc
+ip_address="172.17.145.17"
+user="user_postgresql"
+password="Postgresql"
+port="1433"
+database="HRD"
+
+cs='DRIVER={ODBC Driver 17 for SQL Server};PORT='+port+';SERVER='+ip_address+';DATABASE='+database+';UID='+user+';PWD='+password
+cnn = pypyodbc.connect(cs, unicode_results=True)
+
+
+print(cur)
